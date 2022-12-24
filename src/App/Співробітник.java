d559bf48c291ddbd132a18d3355aa3add735c531
Jollly_Rodger <andrey.teryannikov@gmail.com>
2022-12-24 16:13:49 +0200

    Create class Співробітник

---
+++ src/App/Співробітник.java
@@ -0,0 +1,20 @@
+package App;
+
+
+public class Співробітник {
+
+   private String FullName;
+   private String position;
+   private String email;
+   private String phone;
+   private int age;
+
+
+    public Співробітник(String fullName, String position, String email, String phone, int age) {
+        this.FullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
+}
