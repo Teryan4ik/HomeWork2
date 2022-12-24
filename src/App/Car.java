927c9fe33f957b5e745b064d5720c6688e17a5d8
Jollly_Rodger <andrey.teryannikov@gmail.com>
2022-12-24 16:31:36 +0200

    Create class Car

---
+++ src/App/Car.java
@@ -0,0 +1,24 @@
+package App;
+
+public class Car {
+
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+
+    }
+
+    private void startElectricity() {
+
+    }
+
+    private void startCommand() {
+
+    }
+
+    public void startFuelSystem() {
+//        System.out.println("Hello BMW!");         //test
+
+    }
+}
